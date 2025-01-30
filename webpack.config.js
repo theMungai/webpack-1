8753921e46c6d17d84f1c7705ec9578770eb43c5
theMungai <mungaithesimon@gmail.com>
2025-01-30 12:08:02 +0300

    Bundled JavaScript and handled HTML using HtmlWebpackPlugin

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "mungai.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins:[
+    new HtmlWebpackPlugin({
+        template: "./src/template.html",
+    }),
+  ],
+};
